a0d2d36034b801175914b8f77ca7bb62e529008d
Harish Rajagopal <harish.rajagopals@gmail.com>
2022-08-22 00:15:42 +0200

    Now log level is passed through CLI arg --log-level

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,12 @@ use std::fs::create_dir_all;
 use std::path::Path;
 
 use file_rotate::{compression::Compression, suffix::AppendCount, ContentLimit, FileRotate};
-use gtk::{gio, prelude::*, Application};
+use gtk::{
+    gio,
+    glib::{Char, OptionArg, OptionFlags, VariantDict},
+    prelude::*,
+    Application,
+};
 use gui::Greeter;
 use simplelog::{ConfigBuilder, LevelFilter, WriteLogger};
 
@@ -19,17 +24,28 @@ use crate::constants::{APP_ID, LOG_PATH};
 
 const MAX_LOG_FILES: usize = 3;
 const MAX_LOG_SIZE: usize = 1024 * 1024;
+
+const LOG_LEVEL_CLI_ARG: &str = "log-level";
+// NOTE: A change in this should also change the argument description
 const DEFAULT_LOG_LEVEL: LevelFilter = LevelFilter::Warn;
 
 fn main() {
-    // Setup logging
-    init_logging();
-
     // Register and include resources
     gio::resources_register_include!("compiled.gresource").expect("Failed to register resources.");
 
     // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
+
+    // Specify CLI args and the handler
+    app.add_main_option(
+        LOG_LEVEL_CLI_ARG,
+        Char::from(b'l'),
+        OptionFlags::NONE,
+        OptionArg::String,
+        "The verbosity level of the logs [allowed: off, error, warn, info, debug, trace] [default: warn]",
+        Some("LEVEL"),
+    );
+    app.connect_handle_local_options(handle_cli_args);
 
     // Connect to the "activate" signal of `app`
     app.connect_activate(build_ui);
@@ -38,8 +54,26 @@ fn main() {
     app.run();
 }
 
+/// Handle the CLI arguments
+fn handle_cli_args(_: &Application, args: &VariantDict) -> i32 {
+    let log_level = if let Ok(value) = args.lookup::<String>(LOG_LEVEL_CLI_ARG) {
+        if let Some(value) = value {
+            value
+        } else {
+            String::new()
+        }
+    } else {
+        // Invalid argument type, so return a positive number indicating that the app should crash
+        return 1;
+    };
+    init_logging(&log_level);
+
+    // This denotes that the app should continue to function
+    -1
+}
+
 /// Initialize logging with file rotation
-fn init_logging() {
+fn init_logging(log_level: &str) {
     let log_path = Path::new(LOG_PATH);
     if !log_path.exists() {
         // Create the log directory
@@ -57,14 +91,14 @@ fn init_logging() {
         None,
     );
 
-    // Get the log level from the environment
-    let log_level = match env::var("RUST_LOG").map(|x| x.to_lowercase()).as_deref() {
-        Ok("off") => LevelFilter::Off,
-        Ok("error") => LevelFilter::Error,
-        Ok("warn") => LevelFilter::Warn,
-        Ok("info") => LevelFilter::Info,
-        Ok("debug") => LevelFilter::Debug,
-        Ok("trace") => LevelFilter::Trace,
+    // Parse the log level string
+    let log_level = match log_level {
+        "off" => LevelFilter::Off,
+        "error" => LevelFilter::Error,
+        "warn" => LevelFilter::Warn,
+        "info" => LevelFilter::Info,
+        "debug" => LevelFilter::Debug,
+        "trace" => LevelFilter::Trace,
         _ => DEFAULT_LOG_LEVEL,
     };
 
