a0943c6b99a069c58801d5a027a38c8254edfafa
JohnnyJayJay <johnnyjayjay02@gmail.com>
2023-03-27 14:13:18 +0200

    Exit process after receiving greetd success
    Also move the cache saving process and the "logging in..." message to before the request.

--- src/gui/model.rs
+++ src/gui/model.rs
@@ -449,6 +449,22 @@ impl Greeter {
         for (k, v) in env {
             environment.push(format!("{}={}", k, v));
         }
+
+        self.updates.set_message("Logging in...".to_string());
+
+        if let Some(username) = self.get_current_username() {
+            self.cache.set_last_user(&username);
+            if let Some(session) = session {
+                self.cache.set_last_session(&username, &session);
+            }
+            debug!("Updated cache with current user: {username}");
+        }
+
+        info!("Saving cache to disk");
+        if let Err(err) = self.cache.save() {
+            error!("Error saving cache to disk: {err}");
+        }
+
         // Start the session.
         let response = self
             .greetd_client
@@ -460,20 +476,7 @@ impl Greeter {
         match response {
             Response::Success => {
                 info!("Session successfully started");
-                if let Some(username) = self.get_current_username() {
-                    self.cache.set_last_user(&username);
-                    if let Some(session) = session {
-                        self.cache.set_last_session(&username, &session);
-                    }
-                    debug!("Updated cache with current user: {username}");
-                }
-
-                info!("Saving cache to disk");
-                if let Err(err) = self.cache.save() {
-                    error!("Error saving cache to disk: {err}");
-                }
-
-                self.updates.set_message("Logging in...".to_string());
+                std::process::exit(0);
             }
 
             Response::AuthMessage { .. } => unimplemented!(),
