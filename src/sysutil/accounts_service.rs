261456c9476587766db6e4145e59fc880b8451bd
Sergey Kasmy <sergey.kasmy@gmail.com>
2026-04-11 13:51:53 +0200

    Add missing copyright notice

--- src/sysutil/accounts_service.rs
+++ src/sysutil/accounts_service.rs
@@ -1,3 +1,9 @@
+// SPDX-FileCopyrightText: 2022 Harish Rajagopal <harish.rajagopals@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+//! Barebones D-Bus interface for the org.freedesktop.Accounts D-Bus service.
+
 use zbus::{proxy, zvariant::OwnedObjectPath};
 
 #[proxy(
